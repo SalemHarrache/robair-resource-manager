084f97f379784890eca535e5e321ffb9fd83ae76
Salem Harrache <salem@harrache.info>
2013-02-14 20:57:05 +0100

    add admin backend

---
+++ website/utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+
+def dump_datetime(value):
+    """Deserialize datetime object into string form for JSON processing."""
+    if value is None:
+        return None
+    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
--- website/views/admin.py
+++ website/views/admin.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from flask import Blueprint
-from flask.ext.security import login_required
+from flask import Blueprint, render_template, url_for, redirect, request
+from flask.ext.security import login_required, roles_required
+from ..model import Reservation
+from ..extensions import db
 
 
 app = Blueprint('admin', __name__)
@@ -11,4 +13,15 @@ app = Blueprint('admin', __name__)
 @app.route('/')
 @login_required
 def index():
-    return "admin"
+    return render_template('admin/index.html', items=Reservation.query.all())
+
+
+@app.route('/delete', methods=('POST',))
+@roles_required('admin')
+def delete():
+    reservation_id = request.values.get('id', None)
+    reservation = Reservation.query.filter_by(id=reservation_id).first()
+    if reservation is not None:
+        db.session.delete(reservation)
+        db.session.commit()
+    return redirect(url_for('admin.index'))
